3db6684b3d76ac0f30e7d00b51ebbce535aa71c8
Nozomi Oshima <nozomi.oshima@gmail.com>
2015-04-22 19:12:14 +0900

    update

--- docker_build.sh
+++ docker_build.sh
@@ -6,3 +6,13 @@ dir=`basename $path`
 docker rm -f $dir
 docker build -t $dir .
 
+docker run -i -t -d -p 80:80 --name=$dir \
+  -v $(pwd)/wp-content:/app/wp-content \
+  -v $(pwd)/wp-config.php:/app/wp-config.php \
+  -v $(pwd)/mysql.dump.sql:/mysql.dump.sql \
+  $dir
+
+echo "======================"
+echo "Open $(boot2docker ip)"
+echo "======================"
+
--- docker_start.sh
+++ docker_start.sh
@@ -3,13 +3,5 @@
 path=`pwd`
 dir=`basename $path`
 
-docker run -i -t -d -p 80:80 --name=$dir \
-  -v $(pwd)/wp-content:/app/wp-content \
-  -v $(pwd)/wp-config.php:/app/wp-config.php \
-  -v $(pwd)/mysql.dump.sql:/mysql.dump.sql \
-  $dir
-
-echo "======================"
-echo "Open $(boot2docker ip)"
-echo "======================"
+docker start $dir
 
